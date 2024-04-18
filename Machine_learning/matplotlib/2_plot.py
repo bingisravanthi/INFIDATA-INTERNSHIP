e88a3f2a1fafd496d3ecea1c08468d396653e98f
Sravanthi <bingisravanthi6@gmail.com>
2024-04-18 12:07:49 +0530

    addedmatplotlib

---
+++ Machine_learning/matplotlib/2_plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt#for visualization
+import numpy as np#for array operations
+
+x=np.array([0,6])
+y=np.array([0,10])
+
+plt.plot(x,y,'-0')#creating a plot
+plt.show()#displaying the plot
+# *
+# 0
+# -
+# --
